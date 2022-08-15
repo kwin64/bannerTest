6b6bde5a653e8da3085559e1e5372d159836e0da
kwin64 <kwin11bvb@yandex.by>
2022-08-15 08:55:11 +0300

    gulp init

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,47 @@
 import gulp from "gulp";
 import { path } from "./gulp/config/path.js";
 import { plugins } from "./gulp/config/plugins.js";
+import { copy } from "./gulp/tasks/copy.js";
+import { css } from "./gulp/tasks/css.js";
+import { fontsStyle, otfToTtf, ttfToWoff } from "./gulp/tasks/fonts.js";
+import { html } from "./gulp/tasks/html.js";
+import { images } from "./gulp/tasks/images.js";
+import { js } from "./gulp/tasks/js.js";
+import { reset } from "./gulp/tasks/reset.js";
+import { server } from "./gulp/tasks/server.js";
+import { zip } from "./gulp/tasks/zip.js";
 
 global.app = {
+  isBuild: process.argv.includes("--build"),
+  isDev: !process.argv.includes("--build"),
   path: path,
   gulp: gulp,
   plugins: plugins,
 };
 
-import { copy } from "./gulp/tasks/copy.js";
-import { reset } from "./gulp/tasks/reset.js";
-import { html } from "./gulp/tasks/html.js";
 
 function watcher() {
   gulp.watch(path.watch.files, copy);
   gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.css, css);
+  gulp.watch(path.watch.js, js);
+  gulp.watch(path.watch.images, images);
 }
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 
-const mainTasks = gulp.parallel(copy, html);
-const dev = gulp.series(reset, mainTasks, watcher);
+const mainTasks = gulp.series(
+  fonts,
+  gulp.parallel(copy, html, css, js, images)
+);
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+const build = gulp.series(reset, mainTasks);
+const deployZip = gulp.series(reset, mainTasks, zip);
+// const deployFTP = gulp.series(reset, mainTasks, ftp);
+
+export { dev };
+export { build };
+export { deployZip };
+// export { deployFTP };
 
 gulp.task("default", dev);
+ 
